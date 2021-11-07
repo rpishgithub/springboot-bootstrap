b55c4e46f95f2e8b0b3f8dbb7683390b6cb8e718
rpish <rpishmial@gmail.com>
2021-11-07 22:18:53 +0800

    MyBatis-Plus-04(Pagination)

--- src/main/java/tech/rpish/springbootbootstrap/config/MyBatisConfiguration.java
+++ src/main/java/tech/rpish/springbootbootstrap/config/MyBatisConfiguration.java
@@ -1,7 +1,9 @@
 package tech.rpish.springbootbootstrap.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,8 @@ public class MyBatisConfiguration {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return mybatisPlusInterceptor;
     }
+
 }
--- src/test/java/tech/rpish/springbootbootstrap/SpringbootBootstrapApplicationTests.java
+++ src/test/java/tech/rpish/springbootbootstrap/SpringbootBootstrapApplicationTests.java
@@ -1,11 +1,14 @@
 package tech.rpish.springbootbootstrap;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.rpish.springbootbootstrap.entity.User;
 import tech.rpish.springbootbootstrap.mapper.UserMapper;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest
@@ -47,4 +50,27 @@ public class SpringbootBootstrapApplicationTests {
         userMapper.updateById(user);
     }
 
+    @Test
+    public void selectBatchTest() {
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectByMapTest() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "Jobs");
+        map.put("age", 18);
+        List<User> users = userMapper.selectByMap(map);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void paginationTest() {
+        Page<User> page = new Page<>(1, 3);
+        userMapper.selectPage(page, null);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getRecords());
+        System.out.println(page.getTotal());
+    }
 }
