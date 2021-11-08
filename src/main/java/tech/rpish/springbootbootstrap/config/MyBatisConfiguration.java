f8dd143e0817dbbd319dc30b9a59f676439656ae
rpish <rpishmial@gmail.com>
2021-11-08 11:57:00 +0800

    MyBatis-Plus-06(PerformanceAnalysis)

--- src/main/java/tech/rpish/springbootbootstrap/config/MyBatisConfiguration.java
+++ src/main/java/tech/rpish/springbootbootstrap/config/MyBatisConfiguration.java
@@ -20,5 +20,6 @@ public class MyBatisConfiguration {
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return mybatisPlusInterceptor;
     }
+    
 
 }
